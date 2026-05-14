45f02e59ce0b65a3d2b99f2211bbfc115c21d3e0
ShreejeshG12 <129910787+ShreejeshG12@users.noreply.github.com>
2026-05-13 22:18:58 -0400

    Wrote function to access and build a new list of book titles. Want to revisit once I have learnt how to use map function

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+function getTheTitles(titles = []) {
+    books = []
+    for (let i = 0; i < titles.length; i++) {
+        books.push(titles[i].title);
+    }
+    return books;
 };
 
 // Do not edit below this line
