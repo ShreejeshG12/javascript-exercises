da2314e57dcede891e1eb15def11700b0f29b965
ShreejeshG12 <129910787+ShreejeshG12@users.noreply.github.com>
2026-05-13 22:12:39 -0400

    Wrote a looping fibonacci sequence using prefixed arrays. Want to revisit once learnt deconstruction

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+function fibonacci(a) {
+    let sequence = [0,1,1];
+    let num = Number(a);
+    if (num < 0) return "OOPS";
+    if (num == 0) return 0;
+    if (num == 1) return 1;
+    if (num == 2) return 1;
+    let nextNum = 0
 
+    for (let i = 3; i <= num; i++) {
+        nextNum = sequence[i - 1] + sequence[i - 2];
+        sequence.push(nextNum)
+    }
+    return sequence[sequence.length-1];
+    
 };
 
 // Do not edit below this line
