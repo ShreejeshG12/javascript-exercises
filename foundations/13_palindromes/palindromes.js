a90cf2d459610ddeaa18ab720f135ecc2d65db14
ShreejeshG12 <129910787+ShreejeshG12@users.noreply.github.com>
2026-05-13 21:35:00 -0400

    Wrote a palindrome function that accounts for characters and numbers. Removes special characters and spaces to create cleaned version before looping to reverse then check

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+function isLetter(char) {
+    let regex = /^[a-zA-Z0-9]$/;
+    return regex.test(char);  
+}
 
-};
+function palindromes(text = "") {
+    let cleaned = "";
+    let reversed = "";
+    for (let i = 0; i < text.length; i++) {
+        if (isLetter((text[i]))) {
+            cleaned += text[i].toLocaleLowerCase();
+        }
+    }
+    
+    for (let i = cleaned.length - 1; i >= 0; i--) {
+        reversed += cleaned[i];
+    }
+    
+
+    return cleaned === reversed;
+}
+
 
 // Do not edit below this line
 module.exports = palindromes;
